4d458f5de0b95f861e0c89c41a8fd674b5d5a6aa
Jonathan Lipps <jlipps@gmail.com>
2012-09-18 13:28:57 -0700

    get installer to recognize .sauce_config

--- src/Composer/SausageInstaller.php
+++ src/Composer/SausageInstaller.php
@@ -11,4 +11,29 @@ class SausageInstaller extends LibraryInstaller
     {
         return 'sauce-sausage' === $packageType;
     }
+
+    public function updateCode(PackageInterface $initial, PackageInterface $target)
+    {
+        $path = $this->getInstallPath($initial);
+        $config_file = $path.'/.sauce_config';
+        $contents = null;
+
+        if (is_file($config_file)) {
+            $this->io->write("Backing up Sauce config");
+            $contents = file_get_contents($config_file);
+        }
+
+        parent::updateCode($initial, $target);
+
+        if ($contents) {
+            $this->io->write("Restoring Sauce config");
+            file_put_contents($config_file, $contents);
+        } elseif (getenv('SAUCE_USERNAME') && getenv('SAUCE_ACCESS_KEY')) {
+            $this->io->write("Generating Sauce config based on environment variables");
+            file_put_contents($config_file, getenv('SAUCE_USERNAME').','.
+                getenv('SAUCE_ACCESS_KEY'));
+        } else {
+            $this->io->write("<warning>No Sauce config file found. Please run vendor/bin/sauce_config USERNAME API_KEY</warning>");
+        }
+    }
 }
