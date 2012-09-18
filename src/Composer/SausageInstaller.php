eca248ac34d84cb706faff502e7eabdd5f0dbfb9
Jonathan Lipps <jlipps@gmail.com>
2012-09-18 13:37:26 -0700

    add spaces for pretty output

--- src/Composer/SausageInstaller.php
+++ src/Composer/SausageInstaller.php
@@ -19,21 +19,21 @@ class SausageInstaller extends LibraryInstaller
         $contents = null;
 
         if (is_file($config_file)) {
-            $this->io->write("Backing up Sauce config");
+            $this->io->write("    Backing up Sauce config");
             $contents = file_get_contents($config_file);
         }
 
         parent::updateCode($initial, $target);
 
         if ($contents) {
-            $this->io->write("Restoring Sauce config");
+            $this->io->write("    Restoring Sauce config");
             file_put_contents($config_file, $contents);
         } elseif (getenv('SAUCE_USERNAME') && getenv('SAUCE_ACCESS_KEY')) {
             $this->io->write("Generating Sauce config based on environment variables");
             file_put_contents($config_file, getenv('SAUCE_USERNAME').','.
                 getenv('SAUCE_ACCESS_KEY'));
         } else {
-            $this->io->write("<warning>No Sauce config file found. Please run vendor/bin/sauce_config USERNAME API_KEY</warning>");
+            $this->io->write("<warning>    No Sauce config file found. Please run vendor/bin/sauce_config USERNAME API_KEY</warning>");
         }
     }
 }
